506a313dcec2ab07f60ad92da34e2da75bcd0296
Haktan Çağrı Kodat <134720352+haktan313@users.noreply.github.com>
2025-03-14 17:21:49 +0300

    1

---
+++ Source/gojamk/gojamk.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class gojamk : ModuleRules
+{
+	public gojamk(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
